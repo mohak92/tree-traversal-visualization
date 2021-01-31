ed9af156182cf4ddb281c781b680d0a7d1c795b9
mohak92 <tamhane.mohak@gmail.com>
2021-01-30 23:38:14 -0500

    binary tree data structure

---
+++ src/lib/tree.js
@@ -0,0 +1,15 @@
+
+// The tree data structure file for the app to store nodes and performing traversal
+
+function Node(node) {
+    // Constructor function to create a node
+    this.node = node;
+    this.left = null;
+    this.right = null;
+}
+
+function BinaryTree(val) {
+    // Constructor function to create a Binary tree
+}
+
+export default BinaryTree;
