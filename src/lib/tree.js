0a34d8830bdfafabf7029ef09020ce8310c0a18d
mohak92 <tamhane.mohak@gmail.com>
2021-01-31 13:55:23 -0500

    insert node at specific position

--- src/lib/tree.js
+++ src/lib/tree.js
@@ -24,6 +24,13 @@ function BinaryTree(val) {
         const newNode = new Node(val);
         this.root[dir] = newNode;
     };
+
+    this.insertAt = function(node,val, dir) {
+        // Insert node at specified node to left or right provided as dir
+        const nodePos = this.levelOrderTraverse(node); // Makes use of levelordertraverse api to find the node and insert new node
+        const newNode = new Node(val);
+        nodePos[dir] = newNode;    
+    }
 }
 
 export default BinaryTree;
